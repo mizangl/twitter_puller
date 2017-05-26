37f2f1b386bcda60680fc1fc1efb5e511cce6e66
Martin Zangl <martin.zangl@santexgroup.com>
2017-05-26 09:57:08 -0300

    add pull tweets ui

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,7 +1,16 @@
 -keep class com.mz.twitterpuller.** { *; }
+-keep class com.twitter.sdk.android.** {*;}
 
 -keep class android.support.v4.widget.DrawerLayout { *; }
 -keep class android.support.test.espresso.IdlingResource { *; }
+
+-keep class com.bumptech.glide.** {*;}
+-keep public class * implements com.bumptech.glide.module.GlideModule
+-keep public class * extends com.bumptech.glide.AppGlideModule
+-keep public enum com.bumptech.glide.load.resource.bitmap.ImageHeaderParser$** {
+  **[] $VALUES;
+  public *;
+}
 
 # Proguard rules that are applied to your test apk/code.
 -ignorewarnings
--- app/src/main/java/com/mz/twitterpuller/tweet/TweetsActivity.java
+++ app/src/main/java/com/mz/twitterpuller/tweet/TweetsActivity.java
@@ -4,12 +4,28 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import com.mz.twitterpuller.R;
 import com.mz.twitterpuller.util.BaseActivity;
+import javax.inject.Inject;
 
-public class TweetsActivity extends BaseActivity{
+public class TweetsActivity extends BaseActivity {
+
+  @Inject TweetsPresenter presenter;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     setContentView(R.layout.activity_tweets);
+
+    TweetsFragment fragment =
+        (TweetsFragment) getSupportFragmentManager().findFragmentById(R.id.container);
+
+    if (fragment == null) {
+      fragment = TweetsFragment.newInstance();
+      addFragmentToActivity(getSupportFragmentManager(), fragment, R.id.container);
+    }
+
+    DaggerTweetsComponent.builder()
+        .applicationComponent(getApplicationComponent())
+        .tweetsModule(new TweetsModule(fragment))
+        .build()
+        .inject(this);
   }
 }
